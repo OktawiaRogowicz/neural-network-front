de3eaa87f8431be47c7d21de408c9f3ea42c581f
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2021-12-05 13:43:14 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,153 @@
-import logo from './logo.svg';
 import './App.css';
+import { createRef, useEffect, useRef, useState, useContext } from 'react';
+import Timer from "./Timer";
+import PlayButton from "./PlayButton";
+import SlideToggle from "react-slide-toggle";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import FileSaver from 'file-saver';
 
 function App() {
+
+  const currentWord = String('generated word')
+
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const timerRef = createRef()
+
+  const [isDrawing, setIsDrawing] = useState(false)
+  const [isGameStarted, setIsGameStarted] = useState(false)
+  const [isGameFinished, setIsGameFinished] = useState(false)
+
+  const [isStarted, setIsStarted] = useState(true)
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = 400;
+    canvas.height = 400;
+    canvas.style.width = `400px`;
+    canvas.style.height = `400px`;
+    canvas.style.border = '3px solid gold';
+    canvas.style.backgroundColor = '#fff';
+    canvas.scale = 1;
+
+    const context = canvas.getContext("2d")
+    context.scale(1,1)
+    context.lineCap = "round"
+    context.strokeStyle = "black"
+    context.lineWidth = 10
+    contextRef.current = context;
+  }, [])
+
+  const startDrawing = ({nativeEvent}) => {
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.beginPath()
+    contextRef.current.moveTo(offsetX, offsetY)
+    setIsDrawing(true)
+  }
+
+  const finishDrawing = () => {
+    contextRef.current.closePath()
+    setIsDrawing(false)
+  }
+
+  const draw = ({nativeEvent}) => {
+    if(!isDrawing) {
+      return
+    }
+    const {offsetX, offsetY} = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY)
+    contextRef.current.stroke()
+  }
+
+  const clear = () => {
+    contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
+    contextRef.current.beginPath();
+  }
+
+  const hideStart = () => {
+    setIsStarted(false)
+  }
+
+  const startRound = () => {
+    setIsGameStarted(false)
+    setIsGameFinished(false)
+    clear();
+  }
+
+  const stopTimer = () => {
+    setIsGameFinished(true)
+  }
+
+  const handleDownload = () => {
+    var canvas = document.getElementById("my-canvas");
+    canvas.toBlob(function(blob) {
+      FileSaver.saveAs(blob, "image.png");
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      
+      <SlideToggle duration={800} collapsed
+      render={({ onToggle, setCollapsibleElement, progress }) => (
+        <div>
+          <div className="my-collapsible">
+            <div className="my-collapsible__content" ref={setCollapsibleElement}>
+              <div className="my-collapsible__content-inner"
+              style={{
+                transform: `translateY(${Math.round(20 * (-1 + progress))}px)`
+              }}
+              >
+                <p>
+                  Try and draw
+                </p>
+                <h1>
+                    {currentWord}
+                </h1>
+                <p>
+                  in 15 seconds
+                </p>
+
+                <PlayButton onClick={ () => { onToggle(); setIsGameStarted(true) }}/>
+              </div>
+            </div>
+            <div style={isStarted ? {height: "100vh"} : { height: "100vh", display: 'none' }}>
+              <PlayButton onClick={ () => { onToggle(); hideStart()}}/>
+            </div>
+          </div>
+
+        <div class='game-container' style={ isGameStarted ? {} : {display: 'none'}}>
+          { isGameStarted && <div className="parent timer__content">
+            <div/>
+            <div class='child inline-block-child'>
+              {currentWord}
+            </div>
+            <div/>
+            <div class='child inline-block-child'>
+              <Timer stop={stopTimer} ref={timerRef}/>
+            </div>
+            <div/>
+          </div> }
+
+          <div class='canvas-container' style={ !isGameFinished ? {} : { cursor: 'not-allowed', pointerEvents: 'none' }}>
+            <canvas id='my-canvas'
+              onMouseDown={startDrawing}
+              onMouseUp={finishDrawing}
+              onMouseMove={draw}
+              onDoubleClick={clear}
+              ref={canvasRef}
+            />
+          </div>
+          <div style={ isGameFinished ? {} : { display: 'none' }}>
+            Time has finished!
+            Save your work and continue.
+            <button style={{color: 'gold'}} onClick={handleDownload}> Save</button> 
+            <PlayButton style={{color: 'gold'}} onClick={ () => { onToggle(); startRound() }}/>
+          </div>
+        </div>
+      </div>
+      )}/>
+    </main>
   );
 }
 
---
+++ src/Timer.js
@@ -0,0 +1,66 @@
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import {useContext, useState, useEffect, useRef} from "react";
+
+const red = '#f54e4e';
+const green = '#4aec8c';
+
+function Timer(props) {
+
+  const [isPaused, setIsPaused] = useState(false);
+  const [mode, setMode] = useState('work'); // work/break/null
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const secondsLeftRef = useRef(secondsLeft);
+  const isPausedRef = useRef(isPaused);
+  const modeRef = useRef(mode);
+
+  const workMinutes = 0.25;
+
+  function tick() {
+    secondsLeftRef.current--;
+    setSecondsLeft(secondsLeftRef.current);
+  }
+
+  useEffect(() => {
+
+    secondsLeftRef.current = workMinutes * 60;
+    setSecondsLeft(secondsLeftRef.current);
+
+    const interval = setInterval(() => {
+      if (isPausedRef.current) {
+        return;
+      }
+      if (secondsLeftRef.current === 0) {
+        return props.stop();
+      }
+
+      tick();
+    },1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const totalSeconds = workMinutes * 60;
+  const percentage = Math.round(secondsLeft / totalSeconds * 100);
+
+  const minutes = Math.floor(secondsLeft / 60);
+  let seconds = secondsLeft % 60;
+  if(seconds < 10) seconds = '0'+seconds;
+
+  return (
+    <div style={{width: 100, height: 100}}>
+      <CircularProgressbar
+        value={percentage}
+        text={minutes + ':' + seconds}
+        styles={buildStyles({
+        textColor:'#fff',
+        pathColor:mode === 'work' ? red : green,
+        trailColor:'#fff',
+        strokeWidth: 10,
+      })} />
+    </div>
+  );
+}
+
+export default Timer;
