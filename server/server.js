d6cf9058094aef1e2e39d86fc43ad1a383b616aa
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2021-12-17 23:17:55 +0100

    WIP: fixing index.html on Heroku

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
         console.log(__dirname, 'build', 'index.html');
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
