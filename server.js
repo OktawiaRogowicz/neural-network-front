ffff0a8c1f46144a2ee0fc86a2b6b47c31aaddae
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2021-12-18 20:04:30 +0100

    WIP: fixing Heroku

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var cors = require('cors');
 // const { application } = require('express');
 // const { default: axios } = require('axios');
 
+app.use(express.static(path.join(__dirname, 'client/build')));
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
+    app.get('*', (req,res) =>{
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    });
 }
 
-//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'client/public')));
 app.use(express.json({ limit: '200mb'}));
 app.use(express.urlencoded({ limit: '200mb', extended: true}));
 app.use(cors());
