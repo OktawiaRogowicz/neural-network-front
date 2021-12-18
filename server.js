da9dab130dc5146a4709a329bb8f2014d5a83865
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2021-12-18 19:36:29 +0100

    WIP: fixing heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cors = require('cors');
 if (process.env.NODE_ENV === "production") {
     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
