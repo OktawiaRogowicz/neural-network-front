459616f3b42798c3d366e46e4f35480e671c4373
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2022-01-12 14:55:23 +0100

    WIP: results from neural network visible on App

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tf = require("@tensorflow/tfjs");
+const tfn = require ("@tensorflow/tfjs-node")
 
 const app = express();
 const path = require('path');
@@ -21,8 +22,25 @@ app.use(cors());
 
 app.post('/api/predict', async (req, res) => {
     try {
-        predictSample(req.body.data);
-    res.json({msg: "YAAY"})
+        var result = predictSample(req.body.data);
+        console.log("???");
+        console.log(result);
+
+
+        // var arr = [0.9283097386360168,
+        // 3.825167294024823e-9,
+        // 0.000002357488256166107,
+        // 0.06025977432727814,
+        // 6.390929740973661e-8,
+        // 0.008318196050822735,
+        // 8.327210448966227e-11,
+        // 0.000004928299404127756,
+        // 0.00020167436741758138,
+        // 0.002903217915445566];
+
+        res.json({results: JSON.stringify(result)});
+        console.log("cokolwiek");
+        
     } catch (error) {
 
     }
@@ -51,16 +69,11 @@ app.listen(port, () => {
 
 async function predictSample(sample) {
     var url = 'https://raw.githubusercontent.com/OktawiaRogowicz/neuralNetwork/main/Data%20augmentation/tfjs_model/model.json'
+    //var url = 'https://models-lib.web.app/models/mnist_digits/model.json';
     const model = await tf.loadLayersModel(url);
-    
-    // let result = model.predict(tf.tensor(sample, [1,sample.length])).arraySync();
-    // var maxValue = 0;
-    // var predictedPitch = 10;
-    // for (var i = 0; i < 10; i++) {
-    //   if (result[0][i] > maxValue) {
-    //     predictedPitch = i;
-    //     maxValue = result[0][i];
-    //   }
-    // }
-    // return predictedPitch;
+    const t2 = tf.tensor(sample);
+    const t = tf.tensor(sample, [1, 100, 100, 1]);
+    let result = await model.predict(t).data();
+    console.log(result);
+    return result;
   }
